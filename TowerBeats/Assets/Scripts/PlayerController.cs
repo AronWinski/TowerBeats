d290bdd93d833407c1f13da9d6349e17547a4906
AronWinski <al0043@mix.wvu.edu>
2022-04-02 17:37:36 -0400

    started tower building

---
+++ TowerBeats/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    /*public string tagName = "SpectruObject";
+    public int specStat1 = 700, specStat2 = 750; // this is the spectrum data position.
+    private float spectrumScale, curveEnhancer = 950f;
+    private GameObject[] soundObject;
+
+    // Use this for initialization
+    void Start()
+    {
+        soundObject = GameObject.FindGameObjectsWithTag(tagName);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float[] spectrum = GetComponent<AudioSource>().(1024, 1, FFTWindow.BlackmanHarris);// you can use other calculations than blackmanharris.
+
+        spectrumScale = 1f + ((spectrum[specStat1] + spectrum[specStat2]) * curveEnhancer);
+        for (int i = 0; i <= (soundObject.Length) - 1; i++)
+        {
+            soundObject[i].gameObject.transform.localScale = new Vector3(spectrumScale, spectrumScale, spectrumScale);
+        }
+    }*/
+
+
+}
