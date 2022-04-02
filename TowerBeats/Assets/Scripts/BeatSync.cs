bc12a713a2b72c4c3c2aa2ff0f1690e2a81586d3
AronWinski <al0043@mix.wvu.edu>
2022-04-02 17:42:05 -0400

    last time hopefully

---
+++ TowerBeats/Assets/Scripts/BeatSync.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BeatSync : MonoBehaviour
+{
+
+    // Update is called once per frame
+    /*void Update()
+    {
+        
+    }
+
+    public float bpm = 120f; // Tempo in beats per minute of the audio clip.
+    public float startDelay = 1f; // Number of seconds to delay the start of audio playback.
+    public delegate void AudioStartAction(double syncTime);
+    public static event AudioStartAction OnAudioStart;
+
+    void Start()
+    {
+        double initTime = AudioSettings.dspTime;
+        GetComponent<AudioSource>().PlayScheduled(initTime + startDelay);
+        if (OnAudioStart != null)
+        {
+            OnAudioStart(initTime + startDelay);
+        }
+    }*/
+
+
+}
---
+++ TowerBeats/Assets/Scripts/SubTowerController.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SubTowerController : MonoBehaviour
+{
+    // pulse flag so enemy objects can check for radius
+    bool activeFlag;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        activeFlag = false;
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+
+}
